6cdfa5ab5067b4a70a4d07b8eb1cafcc9fabb0e7
Harshal Patel <patelharshal101@gmail.com>
2022-12-04 17:41:11 -0700

    Query to add movie to DB

--- src/BoundaryDB/MovieDB.java
+++ src/BoundaryDB/MovieDB.java
@@ -32,7 +32,74 @@ public class MovieDB extends Database{
             e.printStackTrace();
         }
         theatre.setMovieList(DBMovies);
-        return DBMovies;
+        return
+        
+        DBMovies;
+    }
+
+
+    public void addMovie(String movieName) {
+        try {
+            String query = "INSERT INTO movies (TheatreID, Name) VALUES (?,?)";
+            PreparedStatement stmt = dbConnect.prepareStatement(query);
+            stmt.setInt(1, 1);
+            stmt.setString(2, movieName);
+            stmt.executeUpdate();
+
+            query = "SELECT * FROM movies WHERE ID = (SELECT MAX(ID) FROM coupons)";
+            Statement s = dbConnect.createStatement();
+            ResultSet res = stmt.executeQuery(query);
+            res.next();
+            int movieId = res.getInt("ID");
+
+
+            query = "INSERT INTO showtimes (MovieID, Time) VALUES (?,?)";
+            stmt = dbConnect.prepareStatement(query);
+
+            stmt.setInt(1, movieId);
+            stmt.setString(2, "12:00");
+            stmt.executeUpdate();
+
+            stmt.setInt(1, movieId);
+            stmt.setString(2, "15:00");
+            stmt.executeUpdate();
+
+            stmt.setInt(1, movieId);
+            stmt.setString(2, "18:00");
+            stmt.executeUpdate();
+
+            stmt.setInt(1, movieId);
+            stmt.setString(2, "20:00");
+            stmt.executeUpdate();
+
+            ArrayList<Integer> showsTimeId = new ArrayList<Integer>();
+
+            //select * from tbl_name order by id desc limit N;
+            query = "SELECT * FROM showtimes ORDER BY ID DESC LIMIT 4";
+            s = dbConnect.createStatement();
+            res = s.executeQuery(query);
+
+            while (res.next()) {
+                showsTimeId.add(res.getInt("ID"));
+            }
+
+            query = "INSERT INTO seats (ShowtimeID, Vacancy, RUser) VALUES (?,?,?)";
+            stmt = dbConnect.prepareStatement(query);
+
+            for(int i = 0; i < showsTimeId.size(); i++){
+                for(int j = 0; j < 20; j++){
+                    stmt.setInt(1, i);
+                    stmt.setBoolean(2, true);
+                    stmt.setBoolean(3, false);
+                    stmt.executeUpdate();
+                }
+            }
+            stmt.close();
+            s.close();
+            res.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
     }
 
     public String getMovieNameFromTheatre(int MovieID,int TheatreID,Theatre theatre){
